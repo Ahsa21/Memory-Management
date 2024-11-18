423e67dab01b5710e1113fe90c7e0cd6f95ec550
Ahmad <Ahmadsalouka@gmail.com>
2024-11-18 21:38:59 +0100

    implement mem_init
    - mem_init was implemented

--- memory_management.c
+++ memory_management.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "memory_manager.h"
 
 typedef struct MemoryBlock {
     struct MemoryBlock *pnt; //pointer to part of memorybool in the memory
@@ -12,8 +12,32 @@ typedef struct MemoryBlock {
 } MemoryBlock;
 
 
-
-
 void* memory_pool = NULL;
 MemoryBlock* Block = NULL;
+
+
+
+
+
+void mem_init(size_t size) {
+
+    memory_pool = malloc(size);
+    Block = malloc(sizeof(MemoryBlock));
+
+    if (memory_pool == NULL || Block == NULL) {
+        printf("memory_pool or Block was not allocated");
+        return;
+    }
+
+    Block -> pnt = memory_pool;
+    Block -> Next = NULL;
+    Block -> size = size;
+    Block -> free = true;
+
+
+}
+
+
+
+
 
---
+++ memory_manager.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void mem_init(size_t size);
+
+void* mem_alloc(size_t size);
+
+void mem_free(void* block);
+
+void* mem_resize(void* block, size_t size);
+
+void mem_deinit();
+
